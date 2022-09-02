37fcacecf4118f61bf6301e995a0dfbd2b1a2c47
alexsilva <geniofuturo@gmail.com>
2022-09-02 15:34:35 -0300

    Setup script v1.

---
+++ setup.py
@@ -0,0 +1,13 @@
+# coding=utf-8
+from distutils.core import setup
+
+setup(
+	name='django-fernet-fieldhasher',
+	version='1.0.0',
+	packages=['fernet_fieldhasher'],
+	url='https://github.com/alexsilva/django-fernet-fieldhasher',
+	license='MIT',
+	author='alex',
+	author_email='',
+	description='Creating encrypted database fields with the algorithm with cryptography.fernet'
+)
