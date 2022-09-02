3d5bc8f9d93a02f554888cb8a3d22a08d3239b95
alexsilva <geniofuturo@gmail.com>
2022-09-02 19:28:44 -0300

    Add readonly field/widget.

--- fernet_fieldhasher/forms/fields.py
+++ fernet_fieldhasher/forms/fields.py
@@ -1,7 +1,13 @@
 # coding=utf-8
 import django.forms as django_forms
+import django.contrib.auth.forms as auth_forms
+from fernet_fieldhasher.forms.widgets import ReadOnlyPasswordHashWidget
 
 
 class PasswordField(django_forms.CharField):
 	"""Field CharField with characters masked in edit."""
 	widget = django_forms.PasswordInput
+
+
+class ReadOnlyPasswordHashField(auth_forms.ReadOnlyPasswordHashField):
+	widget = ReadOnlyPasswordHashWidget
---
+++ fernet_fieldhasher/forms/widgets.py
@@ -0,0 +1,13 @@
+# coding=utf-8
+import django.contrib.auth.forms as auth_forms
+
+
+class ReadOnlyPasswordHashWidget(auth_forms.ReadOnlyPasswordHashWidget):
+	template_name = "fernet_fieldhasher/forms/read_only_password_hash.html"
+
+	def get_context(self, name, value, attrs, **kwargs):
+		"""Convert password from text to hash"""
+		from fernet_fieldhasher.fields import EncryptedPasswordField
+		field = EncryptedPasswordField()
+		value = field.to_python(value)
+		return super().get_context(name, value, attrs, **kwargs)
