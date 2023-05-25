9799bdd73e4aaf6367fee867aaf5a1867bb1135c
alexsilva <geniofuturo@gmail.com>
2023-05-25 13:11:54 -0300

    Defined text field to handle token of undefined length.

--- fernet_fieldhasher/fields.py
+++ fernet_fieldhasher/fields.py
@@ -1,17 +1,19 @@
 # coding=utf-8
+from django.core import validators as dj_validators
+from django.core import checks as dj_checks
+
 from django.db import models
 from fernet_fieldhasher.forms.fields import PasswordField
 from fernet_fieldhasher.hashers import FernetPasswordHasher
 
 
-class FernetCharField(models.CharField):
-	"""CharField with encrypted data"""
-
+class FernetField(models.Field):
 	def __init__(self, key=None, *args, **kwargs):
 		max_length = kwargs.get('max_length', None)
 		self.encoding = kwargs.pop('encoding', None)
 		self.text_length = kwargs.pop('text_length', max_length)
 		super().__init__(*args, **kwargs)
+		self.validators.append(dj_validators.MaxLengthValidator(self.text_length))
 		self.key = key
 		self.fernet = FernetPasswordHasher(key)
 
@@ -48,9 +50,60 @@ class FernetCharField(models.CharField):
 			kwargs['text_length'] = self.text_length
 		return name, path, args, kwargs
 
+	def check(self, **kwargs):
+		return [
+			*super().check(**kwargs),
+			*self._check_text_length_attribute(**kwargs),
+		]
+
+	def _check_text_length_attribute(self, **kwargs):
+		if self.text_length is None:
+			return [
+				dj_checks.Error(
+					"FernetField must define a 'text_length' attribute.",
+					obj=self,
+					id='fields.E120',
+				)
+			]
+		elif (not isinstance(self.text_length, int) or isinstance(self.text_length, bool) or
+		      self.text_length <= 0):
+			return [
+				dj_checks.Error(
+					"'text_length' must be a positive integer.",
+					obj=self,
+					id='fields.E121',
+				)
+			]
+		else:
+			return []
+
+
+class FernetTextField(FernetField, models.TextField):
+	"""TextField with encrypted data"""
+	...
+
+
+class FernetCharField(FernetField, models.CharField):
+	"""CharField with encrypted data"""
+	...
+
 
 class FernetPasswordField(FernetCharField):
 	"""A field that defines an encrypted password"""
+
+	def formfield(self, **kwargs):
+		defaults = {
+			'form_class': PasswordField,
+			'max_length': self.text_length,
+			'strip': False,
+		}
+		defaults.update(kwargs)
+		return super().formfield(**defaults)
+
+
+class FernetPasswordHashField(FernetTextField):
+	"""A field that defines an encrypted password"""
+
 	def formfield(self, **kwargs):
 		defaults = {
 			'form_class': PasswordField,
